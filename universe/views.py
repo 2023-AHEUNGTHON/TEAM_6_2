1f30e0b80b10bc8a5375aaeb9db061f2f9c7f238
LightYe4r <24adrenalin@naver.com>
2023-11-25 17:29:06 +0900

    a

--- universe/views.py
+++ universe/views.py
@@ -37,22 +37,54 @@ class Register(APIView):
                                        school=request.data['school'],
                                        email=request.data['email'],
                                        major=request.data['major'],
+                                       job = request.data['job'],
                                        project=request.data['project'],
                                        password=request.data['password'],
                                        start_date=request.data['start_date'],
                                        end_date=request.data['end_date'])
             user.set_password(request.data['password'])
+            user.is_active = False
             user.save()
+            
+            current_site = get_current_site(request) 
+            domain       = current_site.domain
+            uidb64       = urlsafe_base64_encode(force_bytes(user.pk))
+            token        = account_activation_token.make_token(user)
+            message_data = self.send_message(domain, uidb64, token)
+            self.confirmation_email(message_data ,user.email)
+            
             serializer = UserSerializer(user)
             return Response(serializer.data)
         except IntegrityError as e:
-            return Response({"ERROR": str(e)}, status=400)
-        
+            return Response({"ERROR": str(e)})
+    
+    def send_message(self, domain, uidb64, token):
+        return f"아래 링크를 클릭하면 회원가입 인증이 완료됩니다.\n\n회원가입 링크 : http://{domain}/account/activate/{uidb64}/{token}\n\n감사합니다."
+
+    def confirmation_email(self, message_data, email):
+        # current_site = get_current_site(request) 
+        # message = render_to_string('universe/activation_email.html', {
+        #     'user': user,
+        #     'domain': current_site.domain,
+        #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        #     'token': account_activation_token.make_token(user),
+        # })
+        # current_site = get_current_site(request) 
+        # domain       = current_site.domain
+        # uidb64       = urlsafe_base64_encode(force_bytes(user.pk))
+        # token        = account_activation_token.make_token(user)
+        # message_data = self.send_message(domain, uidb64, token)
+
+        mail_title = "계정 활성화 확인 이메일"
+        mail_to = email
+        email = EmailMessage(mail_title, message_data, to=[mail_to])
+        email.send()
+    
 class Login(APIView):
     def post(self, request, *args, **kwargs):
         user = authenticate(email=request.data['email'], password=request.data['password'])
         if user is None:
-            return Response({'result': 'login fail'}, status=400)
+            return Response({'result': 'login fail'})
         else:
             token = TokenObtainPairSerializer.get_token(user)
             return Response({
@@ -60,6 +92,23 @@ class Login(APIView):
                 'access_token': str(token.access_token),
                 'user_id': user.id,
             })
+     
+class ActivateView(APIView):       
+    def post(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+            auth.login(request, user)
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response({"ERROR": str('계정 활성화 오류')})
+        
 
 class SearchUser(APIView):
     def post(self, request, *args, **kwargs):
@@ -69,6 +118,7 @@ class SearchUser(APIView):
         school = data.get('school')
         email = data.get('email')
         major = data.get('major')
+        job = data.get('job')
         project = data.get('project')
         start_date = data.get('start_date')
         end_date = data.get('end_date')
@@ -107,8 +157,23 @@ class CreatePostView(APIView):
             return Response({"ERROR": str(e)})
 
 class ReadPostView(APIView):
-    def get(self, request, category, *args, **kwargs):
-        post = Post.objects.filter(category=category)
+    def get(self, request, *args, **kwargs):
+        data = request.data
+        title = data.get('title')
+        content = data.get('content')
+        category = data.get('category')
+        user = data.get('user')
+        
+        if title:
+            post = Post.objects.filter(title=title)
+        elif content:
+            post = Post.objects.filter(content=content)
+        elif category:
+            post = Post.objects.filter(category=category)
+        elif user:
+            post = Post.objects.filter(user=User.objects.get(id = user))
+        else:
+            post = Post.objects.all()
         serializer = PostSerializer(post, many=True)
         return Response(serializer.data)
 
@@ -150,7 +215,11 @@ class PostDetailView(APIView):
         return Response(serializer.data)
     
 class MatchingUserView(APIView):
-    def get(self, request, project, major, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
+        data = request.data
+        project = data.get('project')
+        major = data.get('major')
+
         users = User.objects.filter(project=project, major=major)
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
@@ -165,7 +234,10 @@ class DetailUserView(APIView):
         return Response(serializer.data)
     
 class UserProfileView(APIView):
-    def get(self, request, id, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
+        data = request.data
+        id = data.get('id')
+
         user = User.objects.get(id=id)
         serializer = UserSerializer(user)
         return Response(serializer.data)
@@ -174,15 +246,19 @@ class UpdateUserProfileView(APIView):
     def post(self, request, *args, **kwargs):
         data = request.data
         major = data.get('major')
+        job = data.get('job')
         project = data.get('project')
         start_date = data.get('start_date')
         end_date = data.get('end_date')
+        contents = data.get('contents')
         id = data.get('id')
         user = User.objects.get(id=id)
         user.major = major
         user.project = project
         user.start_date = start_date
         user.end_date = end_date
+        user.job = job
+        user.contents = contents
         user.save()
         serializer = UserSerializer(user)
         return Response(serializer.data)
